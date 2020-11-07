f4409c3352bdd5195affd3f439030fa53ce7b4e1
Gerald Versluis <gerald@verslu.is>
2020-11-07 20:00:38 +0100

    Initial commit

---
+++ XFUploadFile.Server/Controllers/UploadFileController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace XFUploadFile.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UploadFileController : ControllerBase
+    { 
+        private readonly ILogger<UploadFileController> _logger;
+        private readonly IWebHostEnvironment _environment;
+
+        public UploadFileController(ILogger<UploadFileController> logger,
+            IWebHostEnvironment environment)
+        {
+            _logger = logger;
+            _environment = environment ?? throw new ArgumentNullException(nameof(environment));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            try
+            {
+                var httpRequest = HttpContext.Request;
+
+                if (httpRequest.Form.Files.Count > 0)
+                {
+                    foreach (var file in httpRequest.Form.Files)
+                    {
+                        var filePath = Path.Combine(_environment.ContentRootPath, "uploads");
+
+                        if (!Directory.Exists(filePath))
+                            Directory.CreateDirectory(filePath);
+
+                        using (var memoryStream = new MemoryStream())
+                        {
+                            await file.CopyToAsync(memoryStream);   System.IO.File.WriteAllBytes(Path.Combine(filePath, file.FileName), memoryStream.ToArray());
+                        }
+
+                        return Ok();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error");
+                return new StatusCodeResult(500);
+            }
+
+            return BadRequest();
+        }
+    }
+}
