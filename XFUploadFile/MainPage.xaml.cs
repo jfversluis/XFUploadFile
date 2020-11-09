768da16f42ced350ba8d90177d4657ce4648396e
Gerald Versluis <gerald@verslu.is>
2020-11-09 11:15:45 +0100

    Implement file upload

--- XFUploadFile/MainPage.xaml.cs
+++ XFUploadFile/MainPage.xaml.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace XFUploadFile
@@ -13,6 +15,22 @@ namespace XFUploadFile
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var file = await MediaPicker.PickPhotoAsync();
+
+            if (file == null)
+                return;
+
+            var content = new MultipartFormDataContent();
+            content.Add(new StreamContent(await file.OpenReadAsync()), "file", file.FileName);
+
+            var httpClient = new HttpClient();
+            var response = await httpClient.PostAsync("http://192.168.1.77:3276/UploadFile", content);
+
+            StatusLabel.Text = response.StatusCode.ToString();
         }
     }
 }
